6cceb6132555eb6f5643dffd131b21d8d545d0f1
Giudi Caglioti <cagliotigiulia.gc@outlook.com>
2022-01-16 19:06:34 +0100

    143

--- ConnessioneDb/view/userList.php
+++ ConnessioneDb/view/userList.php
@@ -5,13 +5,15 @@
 
 <table class='table table-striped'>
   <thead>
-    <tr><th colspan ="5" class="text-center"> Utenti totali: <?=$totalUsers?></th></tr>
+    <tr><th colspan ="6" class="text-center"> Utenti totali: <?=$totalUsers?></th></tr>
     <tr>
       <th class="<?= ($orderby=='UserID')?$orderDir:'' ?>"><a href="<?=$url?>?<?=$orderByQueryString?>&orderDir=<?=$orderDir?>&orderby=UserID"> Id</th>
       <th class="<?= ($orderby=='UserName')?$orderDir:'' ?>"><a href="<?=$url?>?<?=$orderByQueryString?>&orderDir=<?=$orderDir?>&orderby=UserName">Nome</th>
       <th class="<?= ($orderby=='UserCodiceFiscale')?$orderDir:'' ?>"><a href="<?=$url?>?<?=$orderByQueryString?>&orderDir=<?=$orderDir?>&orderby=UserCodiceFiscale">Codice Fiscale</th>
       <th class="<?= ($orderby=='UserEmail')?$orderDir:'' ?>"><a href="<?=$url?>?<?=$orderByQueryString?>&orderDir=<?=$orderDir?>&orderby=UserEmail">Email</th>
       <th class="<?= ($orderby=='UserEta')?$orderDir:'' ?>"><a href="<?=$url?>?<?=$orderByQueryString?>&orderDir=<?=$orderDir?>&orderby=UserEta">Età</th>
+      <th></th>
+      <th></th>
     </tr>
   </thead>
   <tbody>
@@ -27,12 +29,18 @@
           <td><?= $user['UserCodiceFiscale'];?></td>
           <td><a href="mailto:<?= $user['UserEmail'] ?>"><?= $user['UserEmail'];?></a></td>
           <td><?= $user['UserEta'];?></td>
+          <td>
+            <div class="row">
+              <div class="col-4"><a class="btn btn-primary" href="<?=$url?>?action=update&id=<?=$user['UserID']?>"><i class="fas fa-user-edit"></i> Update</a></div>
+              <div class="col-4"><a class="btn btn-danger" onclick="return confirm('Confermi di voler cancellare?')" href="<?=$url?>?action=delete&id=<?=$user['UserID']?>"><i class="fas fa-user-times"></i> Delete</a></div>
+            </div>
+          </td>
         </tr>
 
         <?php
       }?>
       </tbody>
-      <tfoot><tr><td colspan ="5">
+      <tfoot><tr><td colspan ="6">
       <?php require_once 'navigation.php';
       echo '</td></tr></tfoot>';
     }else{?>
